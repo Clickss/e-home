62365ca46defb89a663c0b7842b11afd968f5ef0
vincentpochon <vincentpochon@orange.fr>
2018-05-03 20:17:43 +0200

    authentification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
@@ -24,6 +25,7 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    HttpClientModule,
     routing
   ],
   providers: [
--- src/app/services/authentification.service.ts
+++ src/app/services/authentification.service.ts
@@ -16,9 +16,9 @@ export class AuthenticationService {
     login(username: string, password: string): Observable<boolean> {
         let headers = new Headers({ 'Content-Type': 'application/json' }); // ... Set content type to JSON
         headers.append('Accept', 'application/json');
-        headers.append('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE, PUT');
-        headers.append('Access-Control-Allow-Origin', 'http://localhost:4503');
-        headers.append('Access-Control-Allow-Headers', "X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding");
+        headers.append('Access-Control-Allow-Methods', '*');
+        headers.append('Access-Control-Allow-Origin', '*');
+        headers.append('Access-Control-Allow-Headers', "*");
         console.log("headers1: value" + JSON.stringify(headers));
         let options = new RequestOptions({ headers: headers });
         return this.http.post('http://localhost:8000/api/auth', JSON.stringify({ username: username, password: password }), options)
